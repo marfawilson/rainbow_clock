3753d5a12e597c708500f39e1ebdce015ee1ec4c
Marissa Wilson <marfawilsoncode@gmail.com>
2024-02-28 13:18:19 -0600

    Cleaning up line breaks and empty spaces

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let clock = document.getElementById('clock');
 let body = document.querySelector('body');
 let colors = [ '#4FC3F7', '#29B6F6', '#03A9F4', '#039BE5', '#0288D1', '#0277BD', '#01579B', '#9575CD', '#7E57C2', '#673AB7', '#5E35B1', '#512DA8', '#4527A0', '#311B92', '#7986CB', '#5C6BC0', '#3F51B5', '#3949AB', '#303F9F', '#283593', '#1A237E', '#64B5F6', '#42A5F5', '#2196F3', '#1E88E5', '#1976D2', '#1565C0', '#0D47A1' ];
 
-
 // Array index
 let i = 0;
 
@@ -42,8 +41,9 @@ function currentTime() {
     hours = padTime(hours);
 
     clock.textContent = `${hours}:${mins}:${sec} ${ampm}`;
+
+    changeColor();
 }
 
-// TO-DO: fix clock by displaying live time vs static time
+
 setInterval(currentTime, 1000);
-setInterval(changeColor, 1000);
